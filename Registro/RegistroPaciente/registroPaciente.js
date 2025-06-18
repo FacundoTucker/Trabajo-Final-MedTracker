62aa296c7110aca2fe384917ebfa74b51a1c0bb7
Facundo Tucker <facutucker05@gmail.com>
2025-06-18 12:48:18 -0300

    correcciones y subo RecuperarContraseña

--- Registro/RegistroPaciente/registroPaciente.js
+++ Registro/RegistroPaciente/registroPaciente.js
@@ -1,40 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("registroPacienteForm");
-  const mensajeError = document.getElementById("mensajePaciente");
+  const mensajeError = document.getElementById("mensajeError");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const nombre = document.getElementById("nombre").value.trim();
     const apellido = document.getElementById("apellido").value.trim();
-    const dni = document.getElementById("dni").value.trim();
     const fechaNacimiento = document.getElementById("fechaNacimiento").value.trim();
+    const tipoDocumento = document.getElementById("tipoDocumento").value.trim();
+    const numeroDocumento = document.getElementById("numeroDocumento").value.trim();
+    const domicilio = document.getElementById("domicilio").value.trim();
     const email = document.getElementById("email").value.trim();
+    const telefono = document.getElementById("telefono").value.trim();
     const contraseña = document.getElementById("contraseña").value;
     const contraseñaRepetida = document.getElementById("contraseñaRepetida").value;
 
-    if (!nombre || !apellido || !dni || !fechaNacimiento || !email || !contraseña || !contraseñaRepetida) {
+    //verificamos que los campos esten completos
+    if (!nombre || !apellido || !fechaNacimiento || !tipoDocumento || !numeroDocumento || !domicilio || !email || !telefono || !contraseña || !contraseñaRepetida) {
       mostrarError("Asegúrese de completar todos los campos correctamente.");
       return;
     }
 
+    //verificamos que las contraseñas coincidan
     if (contraseña !== contraseñaRepetida) {
       mostrarError("Las contraseñas no coinciden.");
       return;
     }
 
+    //verificamos que la contraseña cumpla con los requisitos
     if (!validarContraseña(contraseña)) {
       mostrarError("La contraseña debe tener al menos 7 caracteres, una mayúscula y un número.");
       return;
     }
 
-    alert("Registro exitoso!"); //cuando este todo, nos redirige a la pagina correspondiente
+    alert("Registro exitoso!"); //me dirigira a la seccion correspondiente una vez este
     form.reset();
   });
 
   function mostrarError(mensaje) {
     mensajeError.textContent = mensaje;
-    mensajeError.classList.remove("mensajeError");
+    mensajeError.classList.remove("mensajeError")
     mensajeError.classList.add("mostrarMensajeError");
   }
 
@@ -44,10 +50,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let tieneMayuscula = false;
     let tieneNumero = false;
 
+    //logica para verificar una contraseña completa
     for (let i = 0; i < contraseña.length; i++) {
       const char = contraseña[i];
-      if (!isNaN(char)) tieneNumero = true;
-      if (char === char.toUpperCase() && char.match(/[A-Z]/)) tieneMayuscula = true;
+      if (!isNaN(char)) {
+        tieneNumero = true;
+      } else if (char === char.toUpperCase() && char.match(/[A-Z]/)) {
+        tieneMayuscula = true;
+      }
     }
 
     return tieneMayuscula && tieneNumero;
