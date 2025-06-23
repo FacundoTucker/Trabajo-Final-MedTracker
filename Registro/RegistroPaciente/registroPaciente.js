890cd138ea68d6334df68d491a9e31b05848c0b4
LeandroAbelFernandez <leandro.fernandez@gmail.com>
2025-06-22 21:37:46 -0300

    subo carga de historioa clinica, y cambio en registro paciente para prueba

--- Registro/RegistroPaciente/registroPaciente.js
+++ Registro/RegistroPaciente/registroPaciente.js
@@ -27,7 +27,21 @@ document.addEventListener("DOMContentLoaded", () => {
       mostrarError("La contraseña debe tener al menos 7 caracteres, una mayúscula y un número.");
       return;
     }
+        // Guardar paciente en localStorage (solo para pruebas)
+    const nuevoPaciente = {
+      nombre,
+      apellido,
+      dni,
+      fechaNacimiento,
+      email,
+      telefono,
+    };
 
+    const pacientesGuardadosRaw = localStorage.getItem("pacientesDePrueba");
+    const pacientes = pacientesGuardadosRaw ? JSON.parse(pacientesGuardadosRaw) : [];
+
+    pacientes.push(nuevoPaciente);
+    localStorage.setItem("pacientesDePrueba", JSON.stringify(pacientes));
     alert("Registro exitoso!"); //cuando este todo, nos redirige a la pagina correspondiente
     form.reset();
   });
