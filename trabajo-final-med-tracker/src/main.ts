5916caefc933bdc6714c0aa511d1ef0a76aac142
Camila <reygintercamila@gmail.com>
2025-11-30 19:09:32 -0300

    Permite que el especialista actualice solo el estado del turno (Confirmado, Rechazado, etc.).

--- trabajo-final-med-tracker/src/main.ts
+++ trabajo-final-med-tracker/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173', //URL donde corre React
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
 
