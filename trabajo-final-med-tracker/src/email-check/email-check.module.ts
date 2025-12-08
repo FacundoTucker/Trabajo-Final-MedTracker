0744572057805a4beedf36bf126e18cd238af91f
ignacio baldo <nachobaldo14@gmail.com>
2025-12-08 17:11:38 -0300

    corrigiendo bd

--- trabajo-final-med-tracker/src/email-check/email-check.module.ts
+++ trabajo-final-med-tracker/src/email-check/email-check.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { EmailCheckService } from './email-check.service';
+import { EmailCheckService } from '../email-check/email-check.service';
 import { Paciente } from '../paciente/entities/paciente.entity';
 import { Especialista } from '../especialista/entities/especialista.entity';
 
