4474ee1d3bd17f32b90480281e2595cf8ea04c3e
ignacio baldo <nachobaldo14@gmail.com>
2025-12-08 17:40:33 -0300

    bd

--- trabajo-final-med-tracker/src/app.module.ts
+++ trabajo-final-med-tracker/src/app.module.ts
@@ -14,13 +14,14 @@ import { EvolutivoModule } from './evolutivo/evolutivo.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'process.env.MYSQL_HOST',
+      host: process.env.MYSQL_HOST,
       port:  parseInt(process.env.MYSQL_PORT ?? '3306') ,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
 
     EspecialistaModule,
