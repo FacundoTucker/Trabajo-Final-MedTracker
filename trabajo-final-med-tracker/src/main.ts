9a74a4225bedb4ea6b810e684e16d194aaefecd3
ignacio baldo <nachobaldo14@gmail.com>
2025-12-08 16:01:19 -0300

    probando BD

--- trabajo-final-med-tracker/src/main.ts
+++ trabajo-final-med-tracker/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(express.static(join(__dirname, '..', 'public')));
 
   app.enableCors({
-    origin: ["https://trabajo-final-medtracker-react.onrender.com","http://localhost:5173"], //URL donde corre React  //habilita para que venga algo de la nube 
+    origin: ["https://trabajo-final-medtracker.onrender.com","http://localhost:5173"], //URL donde corre React  //habilita para que venga algo de la nube 
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
@@ -22,7 +22,7 @@ async function bootstrap() {
     transform: true, // transforma tipos automaticamente
   }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 
