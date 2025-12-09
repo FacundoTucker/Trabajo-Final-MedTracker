639acdd5013e21b2f5696f6fd8ffe392236ba327
ignacio baldo <nachobaldo14@gmail.com>
2025-12-08 21:33:47 -0300

    probando bd

--- trabajo-final-med-tracker/src/app.module.ts
+++ trabajo-final-med-tracker/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { EspecialistaModule } from './especialista/especialista.module';
 import { PacienteModule } from './paciente/paciente.module';
 import { HistoriaClinicaModule } from './historia-clinica/historia-clinica.module';
@@ -12,16 +15,28 @@ import { EvolutivoModule } from './evolutivo/evolutivo.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port:  parseInt(process.env.MYSQL_PORT ?? '3306') ,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
+    // 1️⃣ Carga automática de .env tanto en local como en Render
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // 2️⃣ Configuración segura para MySQL en Clever Cloud
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        port:  parseInt(process.env.MYSQL_PORT ?? '3306') ,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true, // puedes poner false en producción
+
+        //  🔐 Clever Cloud exige SSL obligatoriamente
+        ssl: { rejectUnauthorized: false },
+      }),
     }),
 
     EspecialistaModule,
@@ -36,4 +51,3 @@ import { EvolutivoModule } from './evolutivo/evolutivo.module';
   providers: [AppService],
 })
 export class AppModule {}
-
